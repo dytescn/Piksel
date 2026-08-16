0e1678393dd1ddd50bf5bf1e051ffc8ef834035d
dowell <apsondu@gmail.com>
2026-08-16 19:15:56 +0800

    移除子模块 piksel_web

--- src/application/browser.rs
+++ src/application/browser.rs
@@ -4,7 +4,9 @@ use wry::{WebView, WebViewBuilder};
 pub fn create_webview(window: Option<&Window>) -> Option<WebView> {
     let window = window?;
 
-    let builder = WebViewBuilder::new().with_initialization_script("window.Piksel_DESKTOP = true;");
+    let builder = WebViewBuilder::new()
+        .with_initialization_script("window.Piksel_DESKTOP = true;")
+        .with_devtools(true);
 
     let url = url::Url::parse("http://127.0.0.1:44944").ok()?;
     let builder = builder.with_url(url);
